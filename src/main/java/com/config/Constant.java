1d887f6e0dfedf3a133ab0740f004d854bcfe2f8
令久 <pujiu.lpj@alibaba-inc.com>
2018-09-11 15:31:36 +0800

    update

--- src/main/java/com/config/Constant.java
+++ src/main/java/com/config/Constant.java
@@ -7,25 +7,25 @@ public class Constant {
     /**
      * 企业corpid, 需要修改成开发者所在企业
      */
-    public static final String CORP_ID = "ding7536bfee6f";
+    public static final String CORP_ID = "***";
     /**
      * 应用的AppKey，登录开发者后台，点击应用管理，进入应用详情可见
      */
-    public static final String APPKEY = "dingklq";
+    public static final String APPKEY = "***";
     /**
      * 应用的AppSecret，登录开发者后台，点击应用管理，进入应用详情可见
      */
-    public static final String APPSECRET = "B7Rjq7sK91jOSXV7g6WOZZqEqPxIOE6d7ddh3";
+    public static final String APPSECRET = "***";
 
     /**
-     * 回调URL加解密用。应用的数据加密密钥，登录开发者后台，点击应用管理，进入应用详情可见
+     * 数据加密密钥。用于回调数据的加密，长度固定为43个字符，从a-z, A-Z, 0-9共62个字符中选取,您可以随机生成
      */
-    public static final String ENCODING_AES_KEY = "54eyi2qdqq9q3qg97mpyw7golb0xqswchgm8sinkex7";
+    public static final String ENCODING_AES_KEY = "***";
 
     /**
-     * 回调URL签名用。应用的签名Token, 登录开发者后台，点击应用管理，进入应用详情可见
+     * 加解密需要用到的token，企业可以随机填写。如 "12345"
      */
-    public static final String TOKEN = "123";
+    public static final String TOKEN = "***";
 
     /**
      * 应用的agentdId，登录开发者后台可查看
@@ -35,5 +35,5 @@ public class Constant {
     /**
      * 审批模板唯一标识，可以在审批管理后台找到
      */
-    public static final String PROCESS_CODE = "PROC-FF6YRLE1N2-K7AKDC6AVAFCM";
+    public static final String PROCESS_CODE = "***";
 }
