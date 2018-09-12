c367d30664e791ec0745a2aa83d85b4d8c9df424
令久 <pujiu.lpj@alibaba-inc.com>
2018-09-12 10:55:57 +0800

    update

--- src/main/java/com/controller/CallbackController.java
+++ src/main/java/com/controller/CallbackController.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 import com.config.Constant;
-import com.dingtalk.oapi.lib.aes.DingTalkEncryptException;
 import com.dingtalk.oapi.lib.aes.DingTalkEncryptor;
 import com.dingtalk.oapi.lib.aes.Utils;
 import com.util.MessageUtil;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import sun.plugin2.message.Message;
 
 import java.util.Map;
 
